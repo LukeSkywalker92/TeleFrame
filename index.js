d90c253714cebffdfbad6c4b76708696fa17bf34
Lukas Scheffler <lukas.scheffler@physik.uni-wuerzburg.de>
2018-09-05 13:14:38 +0200

    added download photo function

--- index.js
+++ index.js
@@ -1,14 +1,43 @@
 require('dotenv').load();
 const Telegraf = require('telegraf')
+const Telegram = require('telegraf/telegram')
+const download = require('image-downloader')
+const moment = require('moment');
+
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
+const telegram = new Telegram(process.env.BOT_TOKEN)
 
-console.log(process.env.BOT_TOKEN)
-
+//Welcome message on bot start
 bot.start((ctx) => ctx.reply('Welcome'))
-bot.help((ctx) => ctx.reply('Send me a sticker'))
-bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.hears(/buy/i, (ctx) => ctx.reply('Buy-buy'))
 
+//Help message
+bot.help((ctx) => ctx.reply('Send me an image.'))
+
+//Download incoming photo
+bot.on('photo', (ctx) => {
+  telegram.getFileLink(ctx.message.photo[ctx.message.photo.length - 1].file_id).then(
+    link => {
+      console.log(link);
+      download.image({
+          url: link,
+          dest: moment().format('x')+'.jpg' // Save to /path/to/dest/photo.jpg
+        })
+        .then(({
+          filename,
+          image
+        }) => {
+          console.log('File saved to', filename)
+        })
+        .catch((err) => {
+          console.error(err)
+        })
+    }
+  )
+});
+
+//Some small conversation
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+
+//Start bot
 bot.startPolling()
