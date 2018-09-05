5a8cd797a3d20d880ba6b4cbed139ec84c3377d4
Luke Scheffler <lukecodewalker92@gmail.com>
2018-09-05 20:56:38 +0200

    added electron + first tests

--- index.js
+++ index.js
@@ -1,43 +1,10 @@
-require('dotenv').load();
-const Telegraf = require('telegraf')
-const Telegram = require('telegraf/telegram')
-const download = require('image-downloader')
-const moment = require('moment');
-
-
-const bot = new Telegraf(process.env.BOT_TOKEN)
-const telegram = new Telegram(process.env.BOT_TOKEN)
-
-//Welcome message on bot start
-bot.start((ctx) => ctx.reply('Welcome'))
-
-//Help message
-bot.help((ctx) => ctx.reply('Send me an image.'))
-
-//Download incoming photo
-bot.on('photo', (ctx) => {
-  telegram.getFileLink(ctx.message.photo[ctx.message.photo.length - 1].file_id).then(
-    link => {
-      console.log(link);
-      download.image({
-          url: link,
-          dest: moment().format('x')+'.jpg' // Save to /path/to/dest/photo.jpg
-        })
-        .then(({
-          filename,
-          image
-        }) => {
-          console.log('File saved to', filename)
-        })
-        .catch((err) => {
-          console.error(err)
-        })
+function resizeToMax(id){
+    myImage = new Image()
+    var img = document.getElementById(id);
+    myImage.src = img.src;
+    if(myImage.width > myImage.height){
+        img.style.width = "100%";
+    } else {
+        img.style.height = "100%";
     }
-  )
-});
-
-//Some small conversation
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-
-//Start bot
-bot.startPolling()
+}
