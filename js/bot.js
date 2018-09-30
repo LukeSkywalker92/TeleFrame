15799a879345869b558ba6895d6b11e2e32c58e3
Lukas Scheffler <luke@lukecodewalker.de>
2018-09-30 12:17:49 +0200

    get bot name for group chats

--- js/bot.js
+++ js/bot.js
@@ -4,67 +4,72 @@ const download = require('image-downloader')
 const moment = require('moment');
 
 var Bot = class {
-  constructor(botToken, imageFolder, imageWatchdog, logger) {
-    var self = this;
-    this.bot = new Telegraf(botToken);
-    this.telegram = new Telegram(botToken);
-    this.logger = logger;
-    this.imageFolder = imageFolder;
-    this.imageWatchdog = imageWatchdog;
+	constructor(botToken, imageFolder, imageWatchdog, logger) {
+		var self = this;
+		this.bot = new Telegraf(botToken);
+		this.telegram = new Telegram(botToken);
+		this.logger = logger;
+		this.imageFolder = imageFolder;
+		this.imageWatchdog = imageWatchdog;
 
-    //Welcome message on bot start
-    this.bot.start((ctx) => ctx.reply('Welcome'));
+		//get bot name
+		this.bot.telegram.getMe().then((botInfo) => {
+			this.bot.options.username = botInfo.username;
+		})
 
-    //Help message
-    this.bot.help((ctx) => ctx.reply('Send me an image.'));
+		//Welcome message on bot start
+		this.bot.start((ctx) => ctx.reply('Welcome'));
 
-    //Download incoming photo
-    this.bot.on('photo', (ctx) => {
-      this.telegram.getFileLink(ctx.message.photo[ctx.message.photo.length - 1].file_id).then(
-        link => {
-          download.image({
-              url: link,
-              dest: this.imageFolder + '/' + moment().format('x') + '.jpg'
-            })
-            .then(({
-              filename,
-              image
-            }) => {
-              this.newImage(filename, ctx.message.from.first_name, ctx.message.caption);
-            })
-            .catch((err) => {
-              this.logger.error(err);
-            })
-        }
-      )
-    });
+		//Help message
+		this.bot.help((ctx) => ctx.reply('Send me an image.'));
 
-    this.bot.catch((err) => {
-      this.logger.error(err)
-    })
+		//Download incoming photo
+		this.bot.on('photo', (ctx) => {
+			this.telegram.getFileLink(ctx.message.photo[ctx.message.photo.length - 1].file_id).then(
+				link => {
+					download.image({
+							url: link,
+							dest: this.imageFolder + '/' + moment().format('x') + '.jpg'
+						})
+						.then(({
+							filename,
+							image
+						}) => {
+							this.newImage(filename, ctx.message.from.first_name, ctx.message.caption);
+						})
+						.catch((err) => {
+							this.logger.error(err);
+						})
+				}
+			)
+		});
 
-    //Some small conversation
-    this.bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+		this.bot.catch((err) => {
+			this.logger.error(err)
+		})
 
-    this.logger.info('Bot created!');
-  }
+		//Some small conversation
+		this.bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 
-  startBot() {
-    //Start bot
-    var self = this;
-    this.bot.startPolling(30, 100, null, () => setTimeout(() => self.startBot(), 30000));
-    this.logger.info('Bot started!');
-  }
+		this.logger.info('Bot created!');
+	}
 
-  newImage(src, sender, caption) {
-    //tell imageWatchdog that a new image arrived
-    this.imageWatchdog.newImage(src, sender, caption);
-  }
+	startBot() {
+		//Start bot
+		var self = this;
+		this.bot.startPolling(30, 100, null, () => setTimeout(() => self.startBot(), 30000));
+		this.logger.info('Bot started!');
+	}
+
+	newImage(src, sender, caption) {
+		//tell imageWatchdog that a new image arrived
+		this.imageWatchdog.newImage(src, sender, caption);
+	}
 
 
 }
 
 /*************** DO NOT EDIT THE LINE BELOW ***************/
 if (typeof module !== "undefined") {
-  module.exports = Bot;
+	module.exports = Bot;
 }
