0870ecb2cca1c6254a9c0bb2586d7fe334ad4821
Lukas Bachschwell <lukas@lbsfilm.at>
2019-04-26 11:02:12 +0200

    reformat main

--- main.js
+++ main.js
@@ -1,45 +1,51 @@
-const {
-  app,
-  BrowserWindow
-} = require('electron')
-const {
-  logger,
-  rendererLogger
-} = require('./js/logger')
-const config = require('./config/config')
-const telebot = require('./js/bot')
-const imagewatcher = require('./js/imageWatchdog')
-const schedules = require('./js/schedules');
+const { app, BrowserWindow } = require("electron");
+const { logger, rendererLogger } = require("./js/logger");
+const config = require("./config/config");
+const telebot = require("./js/bot");
+const imagewatcher = require("./js/imageWatchdog");
+const schedules = require("./js/schedules");
 
 //create global variables
 global.config = config;
 global.rendererLogger = rendererLogger;
 global.images = [];
 
-logger.info('Main app started ...');
+logger.info("Main app started ...");
 
-app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
+app.commandLine.appendSwitch("autoplay-policy", "no-user-gesture-required");
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let win
+let win;
 
 function createWindow() {
   // Create the browser window.
   win = new BrowserWindow({
     width: 1024,
     height: 600
-  })
+  });
 
   win.setFullScreen(config.fullscreen);
   // and load the index.html of the app.
-  win.loadFile('index.html')
+  win.loadFile("index.html");
 
   // get instance of webContents for sending messages to the frontend
-  const emitter = win.webContents
+  const emitter = win.webContents;
 
   // create imageWatchdog and bot
-  var imageWatchdog = new imagewatcher(config.imageFolder, config.imageCount, global.images, emitter, logger);
-  var bot = new telebot(config.botToken, config.imageFolder, imageWatchdog, config.showVideos, logger);
+  var imageWatchdog = new imagewatcher(
+    config.imageFolder,
+    config.imageCount,
+    global.images,
+    emitter,
+    logger
+  );
+  var bot = new telebot(
+    config.botToken,
+    config.imageFolder,
+    imageWatchdog,
+    config.showVideos,
+    logger
+  );
 
   // generate scheduler, when times for turning monitor off and on
   // are given in the config file
@@ -50,38 +56,38 @@ function createWindow() {
   // Open the DevTools.
   // win.webContents.openDevTools()
 
-  bot.startBot()
+  bot.startBot();
 
   // Emitted when the window is closed.
-  win.on('closed', () => {
+  win.on("closed", () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    win = null
-  })
+    win = null;
+  });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on("ready", createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
