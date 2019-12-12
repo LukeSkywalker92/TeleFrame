f5ccb29c3dbed53f91034c62d1920d572c24bfaf
gegu <gegu@empgm.com>
2019-12-12 15:48:31 +0100

    Merging  texts from current config only for fallback

--- js/initLanguage.js
+++ js/initLanguage.js
@@ -5,12 +5,14 @@ module.exports = (config) => {
   let configPath = __dirname + '/../config/';
   if (fs.existsSync(configPath + 'texts.js')) {
     //  load the users language file
-    Object.assign(config, require(configPath + 'texts.js'), config);
+    Object.assign(config, require(configPath + 'texts.js'));
   } else if (config.languageFile && fs.existsSync(configPath + config.languageFile)) {
     //  load the language file definied in config.languageFile
-    Object.assign(config, require(configPath + config.languageFile), config);
+    Object.assign(config, require(configPath + config.languageFile));
   } else {
     // fallback - load the language file for the current environment setting
+    // in addition, always merge the current configuration last in order
+    // to keep the texts of the user settings.
     let envLang = process.env.LANG;
     // including country - 'en_US'
     let langFile = configPath + `i18n/${envLang.substr(0, envLang.indexOf('.'))}.js`;
