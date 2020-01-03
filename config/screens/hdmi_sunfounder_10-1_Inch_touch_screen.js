31393395c05890c056f7bb280da5e361921e4bc6
Malte Misfeldt <malte.misfeldt@aei.mpg.de>
2020-01-03 23:14:32 +0100

    bugfix for pin parameter in screen config file

--- config/screens/hdmi_sunfounder_10-1_Inch_touch_screen.js
+++ config/screens/hdmi_sunfounder_10-1_Inch_touch_screen.js
@@ -9,6 +9,12 @@
  * and a resistor) is required. 
  */
  
+ 
+// THIS VARIABLE NEEDS TO BE CONFIGURED MANUALLY
+// It depends on your individual setup, to which 
+// Pin the optocoupler is connected to.
+var PIN = 21;
+
 var screen = {
     name: "Sunfounder HDMI screen",
     xres: 1280,
@@ -17,10 +23,9 @@ var screen = {
     hasTouch: true,
     hasBacklightCtl: false,
     hasBacklightDimming: false,
-    pin: 1,
-    cmdInit: "gpio mode " + this.pin + "out",
-    cmdBacklightOff: "bash ../tools/screen_switch.sh " + this.pin,
-    cmdBacklightOn:  "bash ../tools/screen_switch.sh " + this.pin,
+    cmdInit: "gpio mode " + PIN + " out",
+    cmdBacklightOff: "bash ./tools/screen_switch.sh " + PIN,
+    cmdBacklightOn:  "bash ./tools/screen_switch.sh " + PIN,
     cmdBacklightDimming: "",
 };
 
